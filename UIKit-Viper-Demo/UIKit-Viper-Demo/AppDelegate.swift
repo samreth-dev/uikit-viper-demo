f2af9c2edb0e021a08d569dea8c6c6c32fb12ae5
samreth <samreth.dev@gmail.com>
2022-11-30 23:50:12 -0600

    first commit

---
+++ UIKit-Viper-Demo/UIKit-Viper-Demo/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  UIKit-Viper-Demo
+//
+//  Created by Samreth Kem on 11/30/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
---
+++ UIKit-Viper-Demo/UIKit-Viper-Demo/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  UIKit-Viper-Demo
+//
+//  Created by Samreth Kem on 11/30/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
