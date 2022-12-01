f72036a6f4839e9da709a7e4c9bc436d2df0fa77
samreth <samreth.dev@gmail.com>
2022-11-30 23:53:38 -0600

    feat: added Todo model

---
+++ UIKit-Viper-Demo/UIKit-Viper-Demo/Entities/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  UIKit-Viper-Demo
+//
+//  Created by Samreth Kem on 11/30/22.
+//
+
+import Foundation
+
+struct Todo: Codable {
+    var userId: Int?
+    var id: Int?
+    var title: String?
+    var completed: Bool?
+}
