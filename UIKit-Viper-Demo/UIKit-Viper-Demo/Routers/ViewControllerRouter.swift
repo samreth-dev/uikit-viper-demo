671aca0b1cfe770af7fccf27f4b75a8b8d98ab5c
samreth <samreth.dev@gmail.com>
2022-12-01 18:11:57 -0600

    feat: added ViewControllerRouter

--- UIKit-Viper-Demo/UIKit-Viper-Demo/Routers/ViewControllerRouter.swift
+++ UIKit-Viper-Demo/UIKit-Viper-Demo/Routers/ViewControllerRouter.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct ViewControllerRouter {
+    
+    func route(viewController: ViewController, text: String?) {
+        let tempViewController = TempViewController(text: text)
+        viewController.navigationController?.pushViewController(tempViewController, animated: true)
+    }
+    
+}
