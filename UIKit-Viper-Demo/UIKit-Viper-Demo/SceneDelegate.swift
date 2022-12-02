cfbe37f3c72ceaba5b238b3b176d2336cda88185
samreth <samreth.dev@gmail.com>
2022-12-01 18:12:18 -0600

    feat: init ScenceDelegate

--- UIKit-Viper-Demo/UIKit-Viper-Demo/SceneDelegate.swift
+++ UIKit-Viper-Demo/UIKit-Viper-Demo/SceneDelegate.swift
@@ -11,9 +11,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScence = (scene as? UIWindowScene) else { return }
+        
+        let apiService = APIService()
+        let apiManager = APIManager(apiService: apiService)
+        let interactor = ViewControllerInteractor(apiManager: apiManager)
+        let router = ViewControllerRouter()
+        let presenter = ViewControllerPresenter(todos: [], interactor: interactor, router: router)
+        let viewController = ViewController(presenter: presenter, cancellable: [])
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window = UIWindow(windowScene: windowScence)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
     }
 
 }
