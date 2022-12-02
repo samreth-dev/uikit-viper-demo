6d174abc769a4c84ca34e380b3b826f2a6057f9d
samreth <samreth.dev@gmail.com>
2022-12-01 18:13:02 -0600

    feat: added ViewController

--- UIKit-Viper-Demo/UIKit-Viper-Demo/Views/ViewController.swift
+++ UIKit-Viper-Demo/UIKit-Viper-Demo/Views/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    private var tableView: UITableView!
+    private let presenter: ViewControllerPresenter
+    private var cancellable: Set<AnyCancellable>
+    
+    init(presenter: ViewControllerPresenter, cancellable: Set<AnyCancellable>) {
+        self.presenter = presenter
+        self.cancellable = cancellable
+        super.init(nibName: nil, bundle: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initViews()
+        setup()
+        binding()
+        presenter.didInit()
     }
-
+    
+    private func initViews() {
+        tableView = UITableView()
+        view.addSubview(tableView)
+    }
+    
+    private func setup() {
+        title = "Todo List"
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    private func binding() {
+        presenter.$todos.receive(on: DispatchQueue.main).sink { [weak self] todos in
+            self?.tableView.reloadData()
+        }
+        .store(in: &cancellable)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        presenter.todos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = presenter.todos[indexPath.row].title
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        presenter.didClick(row: indexPath.row, viewController: self)
+    }
+}
