7af35f756246d288acce5e1b88c26cd9d8810b5b
samreth-dev <samreth.dev@gmail.com>
2022-12-14 22:47:00 -0600

    update: refactors protocol functions codes for ViewControllerPresenter

--- UIKit-Viper-Demo/UIKit-Viper-Demo/Presenters/ViewControllerPresenter.swift
+++ UIKit-Viper-Demo/UIKit-Viper-Demo/Presenters/ViewControllerPresenter.swift
@@ -6,26 +6,33 @@
 //
 
 import Foundation
+import Combine
 
 protocol ViewControllerPresenterProtocol {
     var todos: [Todo] { get set }
     var publisher: Published<[Todo]>.Publisher { get }
+    var cancellable: Set<AnyCancellable> { get set }
+    
     func didInit()
     func didClick(row: Int)
 }
 
-class ViewControllerPresenter: ViewControllerPresenterProtocol {
+class ViewControllerPresenter {
     @Published var todos: [Todo]
     var publisher: Published<[Todo]>.Publisher { $todos }
+    var cancellable: Set<AnyCancellable>
     private let interactor: ViewControllerInteractorProtocol
     private let router: ViewControllerRouterProtocol
     
-    init(todos: [Todo], interactor: ViewControllerInteractorProtocol, router: ViewControllerRouterProtocol) {
+    init(todos: [Todo], cancellable: Set<AnyCancellable>, interactor: ViewControllerInteractorProtocol, router: ViewControllerRouterProtocol) {
         self.todos = todos
+        self.cancellable = cancellable
         self.interactor = interactor
         self.router = router
     }
-    
+}
+
+extension ViewControllerPresenter: ViewControllerPresenterProtocol {
     func didInit() {
         Task { [weak self] in
             do {
